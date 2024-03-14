2ebea290701ee1ff86adac211be73dd77bb8dc48
heinanX <larrii@live.se>
2024-03-14 13:27:20 +0100

    LoadBar component

---
+++ src/app/_components/LoadBar.jsx
@@ -0,0 +1,12 @@
+import { BiLoaderCircle } from "react-icons/bi";
+import { SiFoodpanda } from "react-icons/si";
+
+const LoadBar = () => {
+  return (
+      <span className="text-5xl duration-500 ease-in-out animate-make-a-trip md:text-7xl">
+        <SiFoodpanda />
+      </span>
+  );
+};
+
+export default LoadBar;
