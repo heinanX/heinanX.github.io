d61ae5d3fa65cf4eda98daf01f8cc216255cecd4
heinanX <larrii@live.se>
2024-04-11 13:08:28 +0200

    added cover letter but more about me text

--- src/app/(dashboard)/about/page.jsx
+++ src/app/(dashboard)/about/page.jsx
@@ -6,34 +6,37 @@ import ScrollToBottom from "@/app/_utilities/ScrollToBottom";
 import PersonalitySection from "@/app/_components/PersonalitySection";
 import TechStackSection from "@/app/_components/TechStackSection";
 import ArticleTwoCols from "@/app/_components/ArticleTwoCols";
+import CoverLetterSection from "@/app/_components/CoverLetterSection";
 
 const page = () => {
   return (
     <>
       <div className="relative flex flex-col items-center w-full h-full min-h-screen">
-        <article className="flex justify-center w-full max-w-screen-xl px-10">
+        {/* <article className="flex justify-center w-full max-w-screen-xl px-10">
           <TextImageSection
             image={avatar}
             alt="A professional picture of me smiling into the camera, standing in front of a gradient backdrop"
             component={CoverLetterEng}
           />
-        </article>
-
-        <article className="flex justify-center w-full py-10">
-          <ContactIcons textSize="text-6xl" />
-        </article>
-
-        <div className="flex justify-center w-full bg-custHotpink">
-          <article className="grid items-center w-full min-h-screen grid-cols-1 px-10 py-6 xl:grid-cols-2 max-w-7xl ">
-            <PersonalitySection />
-          </article>
+        </article> */}
+        
+        <ContactIcons textSize="text-6xl" />
+        <div className="flex justify-center w-full bg-custTurq">
+          <ArticleTwoCols
+            component={CoverLetterSection}
+            custCss={"max-w-screen-xl"}
+          />
         </div>
 
-        <ArticleTwoCols component={TechStackSection} />
+        <div className="flex justify-center w-full bg-custHotpink">
+          <ArticleTwoCols component={PersonalitySection} custCss={""} />
+        </div>
 
-        <article className="grid items-center max-w-screen-xl min-h-screen grid-cols-1 px-10 py-6 xl:grid-cols-2">
-          <TechStackSection />
-        </article>
+        <ArticleTwoCols
+          component={TechStackSection}
+          custCss={"max-w-screen-xl"}
+        />
+
         {/*         <h1 className="py-2 text-center dark:text-custHotpink bg-custYellow">
           Location: Stockholm, Sollentuna
         </h1> */}
