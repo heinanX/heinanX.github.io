f1313b79bab15fb59527e9b7ab4e1228a2275865
heinanX <larrii@live.se>
2024-04-19 20:56:40 +0200

    cv

--- src/app/_components/_pageComponents/cvPage/CV.jsx
+++ src/app/_components/_pageComponents/cvPage/CV.jsx
@@ -1,21 +1,28 @@
-"use client"
+"use client";
 import Image from "next/image";
 import StaticBtn from "@/_components/Buttons/StaticBtn";
-import {useState} from "react";
+import { useState } from "react";
 
 const CV = ({ cvImage, href, altText, btnText }) => {
-    const [activeHover, setActiveHover ] = useState(false);
+  const [activeHover, setActiveHover] = useState(false);
   return (
     <div className="flex flex-col items-center">
-      <Image src={cvImage} alt={altText} className={`w-2/3 max-w-80 ${activeHover ? '' : 'opacity-90'}`} />
+      <Image
+        src={cvImage}
+        alt={altText}
+        className={`w-2/3 max-w-80 ${activeHover ? "" : "opacity-90"}`}
+      />
       <a
         href={href}
         alt={`link to ${altText}`}
         target="_blank"
         rel="noopener noreferrer"
       >
-        <div onMouseOver={() => setActiveHover(true)} onMouseOut={() => setActiveHover(false)}>
-        <StaticBtn text={btnText} />
+        <div
+          onMouseOver={() => setActiveHover(true)}
+          onMouseOut={() => setActiveHover(false)}
+        >
+          <StaticBtn text={btnText} />
         </div>
       </a>
     </div>
